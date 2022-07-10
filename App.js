3baf730e14a856ba3c501f7bdfdabb2325c75718
F0427786 alessandrocs <alessandrocs@bb.com.br>
2022-07-10 07:35:26 -0300

    antes do expo-clean

--- App.js
+++ App.js
@@ -2,6 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import * as DevMenu from "expo-dev-menu";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCApdvqPyVbnY87GVos6MgmJNcWOe3mT3o",
+  authDomain: "honra-fgi-peac.firebaseapp.com",
+  projectId: "honra-fgi-peac",
+  storageBucket: "honra-fgi-peac.appspot.com",
+  messagingSenderId: "573020256731",
+  appId: "1:573020256731:web:73c8062174b3727ca44ffe",
+  measurementId: "G-408MEE0092"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const Button = ({ label, onPress }) => (
   <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
     <Text style={styles.buttonText}>{label}</Text>
